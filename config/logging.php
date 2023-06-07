e791856e0632328b99e679734f82cc9cde527245
Mauro <salgadom7503@gmail.com>
2023-06-07 19:25:03 +0100

    n

--- config/logging.php
+++ config/logging.php
@@ -59,7 +59,8 @@ return [
         ],
 
         'single' => [
-            'driver' => 'single',
+            // 'driver' => 'single',
+            'driver' => 'errorlog',
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
             'replace_placeholders' => true,
