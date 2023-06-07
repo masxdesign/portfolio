3d74792829128ea6ff78a56696890e8e3e8d0ccd
Mauro <salgadom7503@gmail.com>
2023-06-07 21:37:52 +0100

    n

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
+        react(),
         laravel({
             input: 'resources/js/app.jsx',
-            ssr: 'resources/js/ssr.js',
+            ssr: 'resources/js/ssr.jsx',
             refresh: true,
         }),
-        react(),
     ],
 });
